5424f49cedb15fa56b39f33cac81b8c0566ffe6d
keffren <sernamateouk@gmail.com>
2024-01-29 14:57:07 +0000

    project 3.2: Integrate parts of module

--- project-3-2/cmd/main.go
+++ project-3-2/cmd/main.go
@@ -1,9 +1,23 @@
 package main
 
+import (
+	"context"
+	"log"
+	"net/http"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/gorilla/mux"
+	"github.com/keffren/go/project-3-2/internal"
+	"github.com/keffren/go/project-3-2/pkg/rest"
+)
+
 func main() {
 
+	// ######################################  DB Services
+
 	// Load the AWS Configuration profile (~/.aws/config)
-	/*cfg, err := config.LoadDefaultConfig(context.TODO())
+	cfg, err := config.LoadDefaultConfig(context.TODO())
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -11,26 +25,25 @@ func main() {
 	// Using the Config value, create the DynamoDB client
 	svc := dynamodb.NewFromConfig(cfg)
 
-	Table := MyDynamoDBTable{
+	var contactsTable internal.MyDynamoDBTable = internal.MyDynamoDBTable{
 		DynamoDbClient: svc,
 		TableName:      "contacts",
 	}
 
-	//table.Init()
-	log.Println(Table) */
-
-	/* newContact := Contact{
-		Id:      2,
-		Name:    "Mateo",
-		Surname: "Zuluaga",
-		Phone:   123456788,
-		City:    "NewYork",
+	contactsHandler := rest.ContactsHandler{
+		Database: &contactsTable,
 	}
 
-	table.AddItem(newContact)
+	// ######################################  Web Server
+	router := mux.NewRouter()
 
-	items_ls, err := table.GetItems()
-	log.Println(err)
-	log.Println(app.ParseListItemsToListContacts(items_ls))*/
+	router.HandleFunc("/api/v1/contacts", contactsHandler.GetContacts).Methods("GET")
+	/* router.HandleFunc("/api/v1/contacts/{id:[0-9]+}", contactsHandler.GetContact).Methods("GET")
+	router.HandleFunc("/api/v1/contacts", contactsHandler.CreateContact).Methods("POST")
+	router.HandleFunc("/api/v1/contacts/{id:[0-9]+}", contactsHandler.UpdateContact).Methods("PUT")
+	router.HandleFunc("/api/v1/contacts", contactsHandler.DeleteContacts).Methods("DELETE")
+	router.HandleFunc("/api/v1/contacts/{id:[0-9]+}", contactsHandler.DeleteContact).Methods("DELETE") */
 
+	log.Print("Listening on port 3001")
+	http.ListenAndServe(":3001", router)
 }
