fa1a200d61e804037bbd1e6659e4235742a00c0d
keffren <sernamateouk@gmail.com>
2024-02-05 10:42:30 +0000

    change scope for access it from other pkg

--- project-4/pkg/database/model.go
+++ project-4/pkg/database/model.go
@@ -4,33 +4,33 @@ import (
 	"database/sql"
 )
 
-type product struct {
+type Product struct {
 	ID    int     `json:"id"`
 	Name  string  `json:"name"`
 	Price float64 `json:"price"`
 }
 
-func (p *product) getProduct(db *sql.DB) error {
+func (p *Product) GetProduct(db *sql.DB) error {
 
 	err := db.QueryRow("SELECT * FROM products WHERE id = $1", p.ID).Scan(&p.Name, &p.Price)
 
 	return err
 }
 
-func (p *product) updateProduct(db *sql.DB) error {
+func (p *Product) UpdateProduct(db *sql.DB) error {
 
 	_, err := db.Exec("UPDATE products SET name=$1, price=$2 WHERE id=$3", p.Name, p.Price, p.ID)
 
 	return err
 }
 
-func (p *product) deleteProduct(db *sql.DB) error {
+func (p *Product) DeleteProduct(db *sql.DB) error {
 
 	_, err := db.Exec("DELETE FROM products WHERE id=$1", p.ID)
 	return err
 }
 
-func (p *product) createProduct(db *sql.DB) error {
+func (p *Product) CreateProduct(db *sql.DB) error {
 
 	// The user won't know the id of product
 	// the DB'll know the id as its is SERIAL value type
@@ -43,9 +43,9 @@ func (p *product) createProduct(db *sql.DB) error {
 	return nil
 }
 
-func getProducts(db *sql.DB) ([]product, error) {
+func GetProducts(db *sql.DB) ([]Product, error) {
 
-	products := []product{}
+	products := []Product{}
 
 	rows, err := db.Query("SELECT id, name, price FROM products")
 
@@ -56,7 +56,7 @@ func getProducts(db *sql.DB) ([]product, error) {
 	defer rows.Close()
 
 	for rows.Next() {
-		p := product{}
+		p := Product{}
 		err := rows.Scan(p.ID, p.Name, p.Price)
 
 		if err != nil {
